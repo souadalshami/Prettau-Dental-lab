4aabafd0d0cf3b143938e3cb9aac98af1073e297
souadalshami <70814584+souadalshami@users.noreply.github.com>
2023-10-08 22:53:54 +0300

    update sections

--- src/components/Certification/Certification.js
+++ src/components/Certification/Certification.js
@@ -34,47 +34,47 @@ const options = {
 function Certification(){
     return(
 
-        <section class="team-two">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-4">
-                        <div class="team-two__left">
-                            <h3 class="section__title-two">Meet our team of creative experts</h3>
-                            <p class="team-two__text">
-                            Lorem ipsum dolor sit amet labore et consectetuer adipiscing elit
-                                sed do eiusmod tempor incididunt.</p>
-                        </div>
+      <section class="team-two">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4">
+                    <div class="team-two__left">
+                        <h3 class="section__title-two">Meet our team of creative experts</h3>
+                        <p class="team-two__text">
+                          Lorem ipsum dolor sit amet labore et consectetuer adipiscing elit
+                            sed do eiusmod tempor incididunt.</p>
                     </div>
-                    <div class="col-xl-8">
-                        <div class="team-two__right">
-                            <OwlCarousel class="team-two__carousel owl-carousel owl-theme thm-owl__carousel" {...options}>
-                                <div class="item">
-                                    <div class="team-two__single">
-                                        <Fancybox>
-                                            <a data-fancybox href={team_2_1} class="img-popup" >
-                                                <div class="team-two__img-box">
-                                                        <div class="team-two__img">
-                                                            <img data-fancybox  src={team_2_1} alt=""/>
-                                                        </div>
-                                                </div>
-                                            </a>
-                                        </Fancybox>
-                                        <div class="team-two__content">
-                                            <div class="team-two__name-box">
-                                                <h3 class="team-two__name">Abral Asha
-                                                </h3>
-                                                <p class="team-two__sub-title">Co-Founder</p>
-                                            </div>
-                                
+                </div>
+                <div class="col-xl-8">
+                    <div class="team-two__right">
+                          <OwlCarousel class="team-two__carousel owl-carousel owl-theme thm-owl__carousel" {...options}>
+                           <div class="item">
+                                <div class="team-two__single">                   
+                                    <Fancybox>
+                                        <a data-fancybox href={team_2_1} class="img-popup" >
+                                          <div class="team-two__img-box">
+                                           <div class="team-two__img">
+                                                   <img data-fancybox  src={team_2_1} alt=""/>
+                                           </div>
+                                           </div>
+                                          </a>
+                                    </Fancybox>
+                                    <div class="team-two__content">
+                                        <div class="team-two__name-box">
+                                            <h3 class="team-two__name">Abral Asha
+                                            </h3>
+                                            <p class="team-two__sub-title">Co-Founder</p>
                                         </div>
+                             
                                     </div>
                                 </div>
-                            </OwlCarousel>
-                        </div>
+                            </div>
+                        </OwlCarousel>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+      </section>
     )
 }
 export default Certification;
